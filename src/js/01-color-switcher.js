81f2267a3b4d86fff72bcb9657a12c64ae0d3451
Andrii Liubenko <lubenkoandrew@gmail.com>
2023-05-15 01:40:50 +0300

    add-task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startButton.addEventListener('click', startChangingBodyColor);
+stopButton.addEventListener('click', stopChangingBodyColor);
+
+function startChangingBodyColor() {
+  startButton.disabled = true;
+  intervalId = setInterval(changeBodyColor, 1000);
+}
+
+function stopChangingBodyColor() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+}
+
+function changeBodyColor() {
+  const body = document.querySelector('body');
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
